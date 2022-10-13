2efd66bed710ee72737c3f275c020f1d44eb4608
BogPomozet <vkerechaninaa@gimail.com>
2022-10-13 16:33:14 +0100

    new file

---
+++ AppRoute.py
@@ -0,0 +1,18 @@
+from distutils.log import debug
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def root():
+    return "The default, 'root' route"
+
+@app.route("/hello/")
+def hello():
+    return "Hello Napier!!! :D"
+
+@app.route("/goodbye/")
+def goodbye():
+    return "Goodbye cruel world :C"
+
+if __name__=="__main__":
+    app.run(host='0.0.0.0', debug=True)
