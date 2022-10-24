29cd82405ffbbb068a1a0a1c4db3528bbbc1c8bb
BogPomozet <vkerechaninaa@gimail.com>
2022-10-24 22:40:17 +0100

    update

--- PythonFlask.py
+++ PythonFlask.py
@@ -1,14 +1,16 @@
+from flask import Flask , url_for
+app = Flask ( __name__ )
 
-from flask import Flask, redirect, url_for
-app = Flask(__name__)
+@app . route ("/")
+def hello () :
+return "Hello Napier"
 
-@app.route("/")
-def hello():
-    return "Hello Napier"
+@app . route (’/static - example /img ’)
+def static_example_img () :
+start = ’<img src =" ’
+url = url_for (’static ’, filename =’vmask .jpg ’)
+end = ’" >’
+return start + url + end , 200
 
-@app.errorhandler(404)
-def page_not_found(error):
-    return "Couldn't find the page you requested.", 404
-    
-if __name__=="__main__":
-    app.run(host='0.0.0.0', debug=True)
+if __name__ == " __main__ ":
+app . run ( host =’0.0.0.0 ’, debug = True )
