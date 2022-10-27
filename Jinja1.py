e5b10905d3c5d34844ffbf7082e6023fc6c821b7
BogPomozet <vkerechaninaa@gimail.com>
2022-10-27 16:35:39 +0100

    test

--- Jinja1.py
+++ Jinja1.py
@@ -3,10 +3,11 @@
 from flask import Flask, render_template
 app=Flask(__name__)
 
-@app.route('/hello/')
+
 @app.route('/hello/<name>')
 def hello (name=None):
-    return render_template('templates/conditional.html', name=name)
+    user={'name' : name}
+    return render_template('templates/hello.html', user==user)
 
 if __name__=="__main__":
     app.run(host='0.0.0.0', debug=True)
