b3d75cf146287c179824a458d9e5b29a4b2c8a7d
BogPomozet <vkerechaninaa@gimail.com>
2022-10-27 16:32:43 +0100

    test

--- Jinja1.py
+++ Jinja1.py
@@ -6,7 +6,7 @@ app=Flask(__name__)
 @app.route('/hello/')
 @app.route('/hello/<name>')
 def hello (name=None):
-    return render_template('conditional.html', name=name)
+    return render_template('templates/conditional.html', name=name)
 
 if __name__=="__main__":
     app.run(host='0.0.0.0', debug=True)
