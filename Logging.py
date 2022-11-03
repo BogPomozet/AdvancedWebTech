8d14228f73a09e4594b64ee461592d54d7287004
BogPomozet <vkerechaninaa@gimail.com>
2022-11-03 16:40:53 +0000

    test

--- Logging.py
+++ Logging.py
@@ -34,7 +34,7 @@ def logs(app):
     file_handler=RotatingFileHandler(log_pathname,maxBytes=1024*1024*10,backupCount=1024)
     file_handler.setLevel(app.config['log_level'])
     formatter=logging.Formatter("%(levelname)s | %(asctime)s | %(module )s | %( funcName )s | %( message )s")
-    file_handler . setFormatter ( formatter )
+    file_handler.setFormatter(formatter)
     app.logger.setLevel(app.config['log_level'])
     app.logger.addHandler(file_handler)
 
