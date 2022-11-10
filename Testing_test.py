a842f5f6a754779c7ae88552aacf153ca6608c14
BogPomozet <vkerechaninaa@gimail.com>
2022-11-10 17:06:28 +0000

    test

--- Testing_test.py
+++ Testing_test.py
@@ -4,7 +4,7 @@ import Testing
 class TestingTest (unittest.TestCase):
     def test_root(self):
         self.app=Testing.app.test_client()
-        out =self.app.get('/')
+        out=self.app.get('/')
         assert '200 OK' in out.content_type
         assert 'charset=utf-8' in out.content_type
         assert 'text/html' in out.content_type
