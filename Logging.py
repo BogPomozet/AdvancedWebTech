0d8b4fb470866111bc3ac55714777656b75918c9
BogPomozet <vkerechaninaa@gimail.com>
2022-11-10 15:52:45 +0000

    test

--- Logging.py
+++ Logging.py
@@ -10,9 +10,9 @@ app=Flask(__name__)
 
 @app.route('/')
 def root():
-    this_route=url_for('.root')
-    app.logger.info("Logging a test message from "+this_route)
-    return "Hello Napier from the configuration testing app (Now with added logging)"
+        this_route=url_for('.root')
+        app.logger.info("Logging a test message from "+this_route)
+        return "Hello Napier from the configuration testing app (Now with added logging)"
 
 def init(app):
     config=configparser()
@@ -35,7 +35,7 @@ def logs(app):
     log_pathname=app.config['log_location']+app.config['log_file']
     file_handler=RotatingFileHandler(log_pathname,maxBytes=1024*1024*10,backupCount=1024)
     file_handler.setLevel(app.config['log_level'])
-    formatter=logging.Formatter("%(levelname)s | %(asctime)s | %(module )s | %( funcName )s | %( message )s")
+    formatter=logging.Formatter("%(levelname)s | %(asctime)s | %(module)s | %(funcName)s | %(message)s")
     file_handler.setFormatter(formatter)
     app.logger.setLevel(app.config['log_level'])
     app.logger.addHandler(file_handler)
